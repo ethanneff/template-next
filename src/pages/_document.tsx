ffff3eb9eccf319ce9c8e242b7670727dd32b42e
Ethan Neff <ethan.neff@eneff.com>
2021-06-10 09:27:31 -0700

    fix: add custom font

---
+++ src/pages/_document.tsx
@@ -0,0 +1,29 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+import {Constants} from '../utils';
+
+export default class DocumentRoute extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return {...initialProps};
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link href={Constants.fontFamily} rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
