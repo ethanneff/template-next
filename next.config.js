bb546a33083823fc7fe61a24f61ed423a3d8d9d2
Ethan Neff <ethan.neff@eneff.com>
2021-06-10 22:05:26 -0700

    fix: attempt github pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
+const basePath = process.env.NODE_ENV === 'production' ? '/template-web' : '';
+
 module.exports = {
+  basePath,
+  assetPrefix: `${basePath}/`,
   images: {
     loader: 'imgix',
   },
