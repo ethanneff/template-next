1656262481022312b94b35faf5a7fb8b17ca839d
Ethan Neff <ethan.neff@eneff.com>
2021-06-16 09:47:36 -0700

    fix: build images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-const basePath = process.env.NODE_ENV === 'production' ? '/template-web' : '';
+const prod = process.env.NODE_ENV === 'production';
+const basePath = prod ? '/template-web' : '';
 
 module.exports = {
   basePath,
   assetPrefix: `${basePath}/`,
+  images: {
+    loader: prod ? 'imgix' : '',
+  },
 };
